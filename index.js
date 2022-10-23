9ac699fb214a941159b0421aa707e77b85d0e54f
Ariana Jugo <arianajugo26@gmail.com>
2022-10-23 19:36:15 -0300

    modified:   Ubicacion.html 	modified:   css/estilos.css 	modified:   habitaciones.html 	new file:   img/Fotos/descripcion.png 	modified:   index.html 	modified:   index.js
    Cambios en habitaciones

---
+++ index.js
@@ -0,0 +1,26 @@
+const images = document.querySelectorAll('.hab1, .hab2, .hab3, .hab4, .hab5, .hab6, .imgicon');
+const imageContainer = document.querySelector('.contenedorimg');
+const containerimg = document.querySelector('.imgicon');
+/*const copy = document.querySelector('copy')*/
+
+images.forEach(image =>{
+    image.addEventListener('click', ()=>{
+      /*  imageContainer.classList.toggle('mover');*/
+        addImage(image.getAttribute('src'), image.getAttribute('alt'));
+      /*  console.log(image.getAttribute('src'));
+        console.log(image.getAttribute('alt'));*/
+        
+    
+    })
+})
+
+const addImage = (srcImage,altImage)=>{
+    imageContainer.classList.toggle('mover');
+    containerimg.src = srcImage;
+    copy.innerHTML = altImage;
+}
+
+imageContainer.addEventListener('click', ()=>{
+    imageContainer.classList.toggle('mover');
+})
+
